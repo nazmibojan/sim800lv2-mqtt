4f802d99895e45bd936acf136826985f9d7ba863
Nazmi Febrian <nazmi.febrian@gmail.com>
2020-01-28 21:06:21 +0700

    - Add readme - Test connect to MQTT broker

---
+++ README.md
@@ -0,0 +1,10 @@
+(Still in progress)
+
+Publish data using MQTT protocol with ESP32 and SIM800L v2
+
+Wiring:
+RX2 (ESP32) -> TXD (SIM800L)
+TX2 (ESP32) -> RXD (SIM800L)
+GND (ESP32) -> GND (SIM800L)
+D5 (ESP32)  -> RST (SIM800L)
+
--- sim800lv2-mqtt.ino
+++ sim800lv2-mqtt.ino
@@ -1,42 +1,24 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-sim800l-publish-data-to-cloud/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
-#include <SoftwareSerial.h>
+#include <WiFi.h>
 
 // Your GPRS credentials (leave empty, if not needed)
 const char apn[]      = "AXIS"; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
 const char gprsUser[] = "axis"; // GPRS User
 const char gprsPass[] = "123456"; // GPRS Password
 
-// SIM card PIN (leave empty, if not defined)
-const char simPIN[]   = ""; 
-
 // Server details
 // The server variable can be just a domain name or it can have a subdomain. It depends on the service you are using
-const char server[] = "hairdresser.cloudmqtt.com"; // domain name: example.com, maker.ifttt.com, etc
-const int  port = 18848;                             // server port number
+const char server[] = "hairdresser.cloudmqtt.com";    // domain name: example.com, maker.ifttt.com, etc
+const int  port = 18848;                              // server port number
 
-// Keep this API Key value to be compatible with the PHP code provided in the project page. 
-// If you change the apiKeyValue value, the PHP file /post-data.php also needs to have the same key 
-//String apiKeyValue = "tPmAT5Ab3j7F9";
-
-// TTGO T-Call pins
+// SIM800L Modem Pins
 #define MODEM_RST            5
-#define MODEM_TX             3
-#define MODEM_RX             2
+#define MODEM_TX             17
+#define MODEM_RX             16
 
 // Set serial for debug console (to Serial Monitor, default speed 115200)
 #define SerialMon Serial
 // Set serial for AT commands (to SIM800 module)
-SoftwareSerial SerialAT(2, 3);
+#define SerialAT  Serial2
 
 // Configure TinyGSM library
 #define TINY_GSM_MODEM_SIM800      // Modem is SIM800
@@ -45,8 +27,8 @@ SoftwareSerial SerialAT(2, 3);
 // Define the serial console for debug prints, if needed
 //#define DUMP_AT_COMMANDS
 
-#include <Wire.h>
 #include <TinyGsmClient.h>
+#include <PubSubClient.h>
 
 #ifdef DUMP_AT_COMMANDS
   #include <StreamDebugger.h>
@@ -58,12 +40,15 @@ SoftwareSerial SerialAT(2, 3);
 
 // TinyGSM Client for Internet connection
 TinyGsmClient client(modem);
+PubSubClient mqtt(client);
+
+unsigned int last_request = 0;
 
 void setup() {
   // Set serial monitor debugging window baud rate to 115200
   SerialMon.begin(115200);
 
-  // Set modem reset, enable, power pins
+  // Set modem reset
   pinMode(MODEM_RST, OUTPUT);
   digitalWrite(MODEM_RST, HIGH);
 
@@ -75,15 +60,7 @@ void setup() {
   // To skip it, call init() instead of restart()
   SerialMon.println("Initializing modem...");
   modem.restart();
-  // use modem.init() if you don't need the complete restart
 
-  // Unlock your SIM card with a PIN if needed
-  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
-    modem.simUnlock(simPIN);
-  }
-}
-
-void loop() {
   SerialMon.print("Connecting to APN: ");
   SerialMon.print(apn);
   if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
@@ -91,31 +68,47 @@ void loop() {
   }
   else {
     SerialMon.println(" OK");
-    
+  }
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+
+  mqtt.setServer(server, port);
+  mqtt.setCallback(mqttCallback);
+}
+
+void loop() {
+  if (millis() - last_request > 50000) {
     SerialMon.print("Connecting to ");
     SerialMon.print(server);
-    if (!client.connect(server, port)) {
+
+    // Connect to MQTT Broker
+    boolean status = mqtt.connect("ADLDev-1", "nglettrq", "RVPcR2AQJEV1");
+
+    if (status == false) {
       SerialMon.println(" fail");
-    }
-    else {
+    } else {
       SerialMon.println(" OK");
-    
-      unsigned long timeout = millis();
-      while (client.connected() && millis() - timeout < 10000L) {
-        // Print available data (HTTP response from server)
-        while (client.available()) {
-          char c = client.read();
-          SerialMon.print(c);
-          timeout = millis();
-        }
-      }
-      SerialMon.println();
-    
-      // Close client and disconnect
-      client.stop();
-      SerialMon.println(F("Server disconnected"));
-      modem.gprsDisconnect();
-      SerialMon.println(F("GPRS disconnected"));
     }
+    
+    last_request = millis();
   }
+
+  mqtt.loop();
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int len) {
+  SerialMon.print("Message arrived [");
+  SerialMon.print(topic);
+  SerialMon.print("]: ");
+  SerialMon.write(payload, len);
+  SerialMon.println();
+
+  // Only proceed if incoming message's topic matches
+//  if (String(topic) == topicLed) {
+//    ledStatus = !ledStatus;
+//    digitalWrite(LED_PIN, ledStatus);
+//    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+//  }
 }
